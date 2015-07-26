363af5429072cde55bb4e17b42b247aa26284ae5
jbirkj <jesperbirkp@gmail.com>
2015-07-26 23:17:51 +0200

    FermLog cleaned up. LCD routines moved to LCD.py. Text logging routines moved to CloudLog.py. Routines for reading 1Wire temeratures ares moved to DS18B20.py

--- CloudLog.py
+++ CloudLog.py
@@ -1,21 +1,6 @@
 # module for logging data somwhere
 import csv, datetime
 
-def GoogleSubmit(value1, value2, value3):
-    try:
-        gs = gspread.login('jesperbirkp@gmail.com', 'zxkfdgmtpslbqpzg')
-    except:
-        print('fail')
-        sys.exit
-    wks = gs.open("TempLog1").sheet1
-    
-    #writing to Google sheet
-    values = [ datetime.datetime.now(), 'sensor', value1, value2, value3]
-    wks.append_row(values)
-    
-    #wks.close() 
-
-
 def textLog(FN, T, V1, V2, V3):
 	fileH = open(	FN, 'a', newline='') #open with append option to add data to existing file
 	fObj = csv.writer(fileH)
